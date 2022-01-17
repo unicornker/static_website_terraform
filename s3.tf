4ba4ffd6ef9a5b498f4608c1e29df2d6e041306d
unicornker <86202395+unicornker@users.noreply.github.com>
2022-01-18 01:06:53 +0200

    Create s3.tf

---
+++ s3.tf
@@ -0,0 +1,39 @@
+resource "aws_s3_bucket" "www_bucket" {
+  bucket = "www.${var.bucket_name}"
+  acl = "public-read"
+  policy = templatefile("templates/s3-policy.json", { bucket = "www.${var.bucket_name}" })
+
+  cors_rule {
+    allowed_headers = ["Authorization", "Content-Length"]
+    allowed_methods = ["GET", "POST"]
+    allowed_origins = ["https://www.${var.domain_name}"]
+    max_age_seconds = 3000
+  }
+
+  website {
+    index_document = "index.html"
+    error_document = "404.html"
+  }
+
+  tags = var.common_tags
+}
+
+
+resource "aws_s3_bucket_object" "single_file" {
+    bucket = aws_s3_bucket.www_bucket.id
+    key = "index.hmtl"
+    source = "templates/index.html"
+#    etag = filemd5("index.html")
+}
+
+resource "aws_s3_bucket" "root_bucket" {
+  bucket = var.bucket_name
+  acl = "public-read"
+  policy = templatefile("templates/s3-policy.json", { bucket = var.bucket_name })
+
+  website {
+    redirect_all_requests_to = "https://www.${var.domain_name}"
+  }
+
+  tags = var.common_tags
+}
